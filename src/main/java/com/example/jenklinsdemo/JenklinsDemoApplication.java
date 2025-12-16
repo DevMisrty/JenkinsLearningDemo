8c98db63d84e47f50ffb56d34387d2e251e35479
DevMisrty <dev.mistry.agileinfoways@gmail.com>
2025-12-16 10:49:50 +0530

    initial commit

---
+++ src/main/java/com/example/jenklinsdemo/JenklinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenklinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenklinsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenklinsDemoApplication.class, args);
+    }
+
+}
